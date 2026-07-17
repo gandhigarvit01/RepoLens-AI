e887e1604e1a041d87ba8700613f99553bc6f5bb
gandhigarvit01 <gandhigarvit2712@gmail.com>
2026-07-18 02:29:10 +0530

    Fix Qdrant Cloud authentication

--- server/config/qdrant.js
+++ server/config/qdrant.js
@@ -1,7 +1,9 @@
 const { QdrantClient } = require("@qdrant/js-client-rest");
 
 const client = new QdrantClient({
-    url: process.env.QDRANT_URL
+    url: process.env.QDRANT_URL,
+    apiKey: process.env.QDRANT_API_KEY,
+    checkCompatibility: false
 });
 
 async function createCollection(collectionName) {
